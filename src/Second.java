f85baca3ac50c450877ce6ffdef2720c50b4ddb1
T5800 <whao.do@gmail.com>
2013-05-11 11:28:38 +0800

    add Second.java

--- src/Second.java
+++ src/Second.java
@@ -1,4 +1,8 @@
 public class Second {
+	private String password;
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public static void main(String[] args) {
 		System.out.println("Second");
 	}
