9b7992f3ae731f238d89727303674cd6c0d39758
T5800 <whao.do@gmail.com>
2013-05-18 21:26:26 +0800

    new groovy

---
+++ src/com/Closure.groovy
@@ -0,0 +1,15 @@
+class Person {
+	def name
+	def age
+}
+def  list =  [new Person(name:"T5800",age:24)
+		new Person(name:"whao",age:12)]
+def isNotNull = {
+	str, Closure closure ->
+	if(str != null ) {
+		closure.call()
+	}
+}
+isNotNull("yes") {
+	println "hello, groovy"
+}
